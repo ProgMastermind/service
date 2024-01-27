11e22add88675c83430caa2015671e11ff582927
ProgMastermind <surya.21mis7126@vitap.student.ac.in>
2024-01-27 15:43:59 +0530

    commit 12

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -4,13 +4,17 @@ import (
 	"ardanlabs/service/foundation/logger"
 	"context"
 	"errors"
+	"expvar"
 	"fmt"
-	"github.com/ardanlabs/conf/v3"
+	"net/http"
+	_ "net/http/pprof"
 	"os"
 	"os/signal"
 	"runtime"
 	"syscall"
 	"time"
+
+	"github.com/ardanlabs/conf/v3"
 )
 
 var build = "develop"
@@ -60,7 +64,7 @@ func run(ctx context.Context, log *logger.Logger) error {
 			IdleTimeout        time.Duration `conf:"default:120s"`
 			ShutdownTimeout    time.Duration `conf:"default:20s,mask"`
 			APIHost            string        `conf:"default:0.0.0.0:3000"`
-			DebugHost          string        `conf:"default:0.0.0.0:4000"`
+			DebugHost          string        `conf:"default:0.0.0.0:4003"`
 			CORSAllowedOrigins []string      `conf:"default:*"`
 		}
 	}{
@@ -91,6 +95,18 @@ func run(ctx context.Context, log *logger.Logger) error {
 		return fmt.Errorf("generating config for output: %w", err)
 	}
 	log.Info(ctx, "startup", "config", out)
+
+	expvar.NewString("build").Set(build)
+	// -------------------------------------------------------------------------
+	// Start Debug Service
+
+	go func() {
+		log.Info(ctx, "startup", "status", "debug v1 router started", "host", cfg.Web.DebugHost)
+
+		if err := http.ListenAndServe(cfg.Web.DebugHost, http.DefaultServeMux); err != nil {
+			log.Error(ctx, "shutdown", "status", "debug v1 router closed", "host", cfg.Web.DebugHost, "msg", err)
+		}
+	}()
 
 	// ------------------------------------------------------------
 
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module ardanlabs/service
 go 1.21.6
 
 require github.com/ardanlabs/conf/v3 v3.1.7
+
+require github.com/google/go-cmp v0.6.0 // indirect

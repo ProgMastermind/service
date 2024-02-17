4ae890a592180725406c4d3808a40ffd97a06d75
ProgMastermind <surya.21mis7126@vitap.student.ac.in>
2024-02-17 10:39:40 +0530

    commit 43

---
+++ business/data/dbmigrate/dbmigrate.go
@@ -0,0 +1,76 @@
+package dbmigrate
+
+import (
+	"context"
+	"database/sql"
+	_ "embed"
+	"errors"
+	"fmt"
+
+	database "ardanlabs/service/business/data/dbsql/pgx"
+
+	"github.com/ardanlabs/darwin/v3"
+	"github.com/ardanlabs/darwin/v3/dialects/postgres"
+	"github.com/ardanlabs/darwin/v3/drivers/generic"
+	"github.com/jmoiron/sqlx"
+)
+
+var (
+	//go:embed sql/migrate.sql
+	migrateDoc string
+
+	//go:embed sql/seed.sql
+	seedDoc string
+)
+
+// Migrate attempts to bring the database up to date with the migrations
+// defined in this package.
+func Migrate(ctx context.Context, db *sqlx.DB) error {
+	if err := database.StatusCheck(ctx, db); err != nil {
+		return fmt.Errorf("status check database: %w", err)
+	}
+
+	driver, err := generic.New(db.DB, postgres.Dialect{})
+	if err != nil {
+		return fmt.Errorf("construct darwin driver: %w", err)
+	}
+
+	// darwin reads the migration file and parse it into set of objects that this
+	// darwin knew how to read against this driver for the database and essentially
+	// applying in to the database
+	d := darwin.New(driver, darwin.ParseMigrations(migrateDoc))
+	return d.Migrate()
+}
+
+// Seed runs the seed document defined in this package against db. The queries
+// are run in a transaction and rolled back if any fail.
+func Seed(ctx context.Context, db *sqlx.DB) (err error) {
+	if err := database.StatusCheck(ctx, db); err != nil {
+		return fmt.Errorf("status check database: %w", err)
+	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if errTx := tx.Rollback(); errTx != nil {
+			if errors.Is(errTx, sql.ErrTxDone) {
+				return
+			}
+			err = fmt.Errorf("rollback: %w", errTx)
+			return
+		}
+	}()
+
+	if _, err := tx.Exec(seedDoc); err != nil {
+		return fmt.Errorf("exec: %w", err)
+	}
+
+	if err := tx.Commit(); err != nil {
+		return fmt.Errorf("commit: %w", err)
+	}
+
+	return nil
+}
